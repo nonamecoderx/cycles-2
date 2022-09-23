ff1dbd745236753f0f0a0a3e07f22b4696b23fa5
Иван <ivankiseliov@rambler.ru>
2022-09-23 20:15:18 +0300

    dz2-2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main {
     public static void main(String[] args) {
         int totalCoin = 15_000;
         int i = 0;
-        for ( i = 1; totalCoin < 12_000_000; i++) {
+        for (i = 1; totalCoin < 12_000_000; i++) {
             totalCoin = totalCoin + totalCoin * 7 / 100 / 12;
-            System.out.println("Месяц " + i + ", сумма накоплений " + totalCoin);
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + ", сумма накоплений " + totalCoin);
+            }
         }
     }
 }
