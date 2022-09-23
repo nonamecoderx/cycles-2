6dd4f08dd04125b8cd2aaa1338245043a36b1f9b
Иван <ivankiseliov@rambler.ru>
2022-09-23 21:16:44 +0300

    dz2-4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int totalCoin = 15_000;
-        int i = 0;
-        int totalYears = 9;
-        int totalMounth = totalYears * 12;
-        for (i = 1; i <= totalMounth; i++) {
-            totalCoin = totalCoin + totalCoin * 7 / 100 / 12;
-            if (i % 6 == 0) {
-                System.out.println("Месяц " + i + ", сумма накоплений " + totalCoin);
+        int friday = 5;
+        int date = 1;
+        while ( date <= 31 ) {
+            date = date + 1;
+            if (date == friday) {
+                System.out.println("Сегодня пятница, " + date + " число. Необходимо подготовить отчет.");
+                friday = friday + 7;
             }
         }
     }
