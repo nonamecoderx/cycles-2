9eef2238490e17bd8ee1c57d99d46032b1431351
Иван <ivankiseliov@rambler.ru>
2022-09-23 20:21:19 +0300

    dz2-3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int totalCoin = 15_000;
         int i = 0;
-        for (i = 1; totalCoin < 12_000_000; i++) {
+        int totalYears = 9;
+        int totalMounth = totalYears * 12;
+        for (i = 1; i <= totalMounth; i++) {
             totalCoin = totalCoin + totalCoin * 7 / 100 / 12;
             if (i % 6 == 0) {
                 System.out.println("Месяц " + i + ", сумма накоплений " + totalCoin);
