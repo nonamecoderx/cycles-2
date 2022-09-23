3282e5744e3a05709cfd4bee1f4f38238a3ba647
Иван <ivankiseliov@rambler.ru>
2022-09-23 14:28:17 +0300

    dz2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int salary = 15000;
-        int total = 0;
-        int i = 0;
-        while ( total < 2459000 ) {
-            total = total + total / 100;
-            total = total + salary;
-            //System.out.println("Месяц " + i + " сумма - " + total);
-            i =i+1;
-            System.out.println("Месяц " + i + " сумма накоплений " + total);
+        int a = 1;
+        int b = 10;
+        while (a <= 10) {
+            System.out.print(a + " ");
+            a = a + 1;
+        }
+        while (b >= 1) {
+            System.out.print(b + " ");
+            b = b - 1;
         }
     }
 }
